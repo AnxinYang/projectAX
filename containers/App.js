7c76dc2134781b0d39a64b191670074ea9408252
AnxinYang <32281036+AnxinYang@users.noreply.github.com>
2018-03-28 21:34:36 -0700

    setup

---
+++ containers/App.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Anxin Yang on 3/28/2018.
+ */
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render () {
+        return <p>This is my new react app</p>
+    }
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Anxin Yang on 3/28/2018.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+import App from './containers/App';
+
+ReactDOM.render(<App />, document.getElementById('app'))
