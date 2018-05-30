e8eb689c6017f349156e6a3cd185efdc9ff34b2d
Anxin <anxinyang114@gmail.com>
2018-05-29 17:21:16 -0700

    add menu

--- src/index.js
+++ src/index.js
@@ -11,28 +11,52 @@ var data={
 var root = new AXDOM('div','ax_root',document.getElementById('app'));
 root.style('font-size','12px');
 var header = root.append('div','header')
+    .attr('onselectstart','return false;')
     .style('display','flex')
     .style('position','absolute')
     .style('background','#70a1ff')
     .style('width','100vw')
     .style('height','3em')
     .style('z-index','10');
-var headerItems = ['Home','Playground','About'];
+
+var headerItems = ['▤','Home','Playground','About'];
+var index = 0;
 headerItems.forEach(function (item) {
-    header.append('div','header_'+item)
+    headerItems[index++] = header.append('div', 'header_' + item)
         .content(item)
         .appendClass('xx')
-        .style('color','white')
-        .style('padding','1em 0.5em')
-        .style('cursor','pointer')
-        .style('transition','0.3s')
-        .on('mouseover',function () {
-            this.style('background','#1e90ff');
+        .style('color', 'white')
+        .style('padding', '1em 0.5em')
+        .style('cursor', 'pointer')
+        .style('transition', '0.3s')
+        .on('mouseover', function () {
+            this.style('background', '#1e90ff');
         })
-        .on('mouseleave',function () {
-            this.style('background','');
+        .on('mouseleave', function () {
+            this.style('background', '');
         });
 });
+var headerMenuButton = headerItems[0];
+headerMenuButton.style('position','relative');
+var menuContainer = headerMenuButton.append('div','menuContainer');
+
+menuContainer.style('position','absolute')
+    .style('width','256px')
+    .style('height', '0px')
+    .style('background','#70a1ff')
+    .style('top','100%')
+    .style('left','0')
+    .style('transition', '0.3s');
+headerMenuButton.on('click',function (d,e) {
+   let hasOpen = this.hasOpen || false;
+   if(hasOpen){
+       menuContainer.style('height','0');
+   }else{
+       menuContainer.style('height','calc(100vh - 3em)');
+   }
+   this.hasOpen = !hasOpen;
+});
+
 var version = root.append('p','version')
     .style('position','fixed')
     .style('bottom','0px')
