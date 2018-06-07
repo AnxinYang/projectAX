ef6380d9a3f870eac58bafbb85d448f0838cb7b6
AnxinYang <32281036+AnxinYang@users.noreply.github.com>
2018-06-07 01:15:23 -0700

    a

--- src/home.js
+++ src/home.js
@@ -31,7 +31,6 @@ var headLine = mainContent.append('h1','headLine')
     });
 var homeHeadLineAnimation = AXR.append('home_headLine_animation')
     .attr('freq',100)
-    .attr('excutionTimes',5)
     .attr('action',function () {
         let odd = Math.random()*100;
         if(odd>50){
