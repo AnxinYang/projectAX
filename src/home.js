30d4ee5e5399038a7528f38a1c98d258ebd65ee4
AnxinYang <32281036+AnxinYang@users.noreply.github.com>
2018-06-06 23:51:39 -0700

    home backgrond competed

--- src/home.js
+++ src/home.js
@@ -39,42 +39,62 @@ var homeHeadLineAnimation = AXR.append('home_headLine_animation')
         }
     })
     .insert();
-var circleNum = 100;
+var circleNum = 150;
 var circleList = [];
 var origin = {
     x: window.innerWidth/2,
     y: window.innerHeight/2
 };
 for(var i=0;i<circleNum;i++){
-    let x = Math.random()*window.innerWidth;
-    let y = Math.random()*window.innerWidth;
+    let x = Math.random()*window.innerWidth+10;
+    let y = Math.random()*window.innerHeight+10;
     let circle = mainContent.append('div')
         .style('position','absolute')
-        .style('width','32px')
-        .style('height','32px')
+        .style('width','20px')
+        .style('height','20px')
         .style('border-radius','50%')
         .style('background','#eccc68')
-        //.style('transition','15s')
+        .style('transition','0.1s linear')
         .style('box-shadow','0 0 10px #eccc68')
         .style('opacity', '0')
         .style('transform','translate('+x+'px,'+y+'px)');
     circle.x = x;
     circle.y = y;
+    circle.dx = Math.random()-0.5;
+    circle.dy = Math.random()-0.5;
+    circle.ds = 0.01;
+    circle.scale =  Math.random();
+    circle.counter = 10;
     let circleAn = AXR.append('home_circle_animation')
-        .attr('freq',1)
+        .attr('freq',5)
         .attr('action',function () {
-            let dx = Math.random()*20-10;
-            let dy = Math.random()*20-10;
-            let scale = Math.random()*1.5;
-            let x = (circle.x2 || circle.x)+dx;
-            let y = (circle.y2 || circle.y)+dy;
-            circle.x2=x;
-            circle.y2=y;
-            circle.style('transform','translate('+x+'px,'+y+'px) scale('+scale+')')
-                .style('opacity', (Math.random()*0.5)+0.5);
-            if((x>window.innerWidth || x<0)|| (y>window.innerHeight || y<0)){
-                circle.x2=circle.x;
-                circle.y2=circle.y;
+            let scale = circle.scale;
+            circle.x+=circle.dx;
+            circle.y+=circle.dy;
+
+            circle.style('transform','translate('+circle.x+'px,'+circle.y+'px) scale('+scale+')')
+                .style('opacity', scale)
+                .style('box-shadow','0 0 '+(Math.random()*10+10)+'px #eccc68');
+            if(circle.x>window.innerWidth){
+                circle.dx = -(Math.random()*0.5+0.1);
+            }else if(circle.x<0){
+                circle.dx = Math.random()*0.5+0.1;
+            }
+            if(circle.y>window.innerHeight){
+                circle.dy = -(Math.random()*0.5+0.1);
+            }else if(circle.y<0){
+                circle.dy = Math.random()*0.5+0.1;
+            }
+            circle.counter--;
+            if(circle.counter<=0){
+                circle.counter=10;
+                circle.scale+=circle.ds;
+                if(circle.scale>=1){
+                    circle.ds=-0.01;
+                }
+                if(circle.scale<=0){
+                    circle.ds=0.01;
+                }
             }
         }).insert();
 };
