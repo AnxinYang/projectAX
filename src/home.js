4c0647db3cfd718ded166012c4a2f962d9d9ee80
AnxinYang <32281036+AnxinYang@users.noreply.github.com>
2018-06-07 23:56:46 -0700

    Home page done

--- src/home.js
+++ src/home.js
@@ -20,26 +20,124 @@ var headLine = mainContent.append('h1','headLine')
     .style('bottom','33%')
     .style('margin','auto')
     .style('font-size','64px')
-    .style('transition', '5s')
+    .style('transition', '1s')
     .style('text-shadow','0 0 10px #70a1ff')
     .style('z-index',1)
     .on('mouseover',function () {
-        this.style('text-shadow','0 0 30px #70a1ff')
+        this.style('text-shadow','0 0 30px #eccc68')
     })
     .on('mouseleave',function () {
         this.style('text-shadow','0 0 10px #70a1ff')
     });
 var homeHeadLineAnimation = AXR.append('home_headLine_animation')
-    .attr('freq',100)
+    .attr('freq',25)
     .attr('action',function () {
         let odd = Math.random()*100;
         if(odd>50){
-            headLine.style('text-shadow','0 0 20px #eccc68')
+            headLine.style('text-shadow','0 0 30px #eccc68')
         }else{
             headLine.style('text-shadow','0 0 10px #70a1ff')
         }
     })
     .insert();
+
+var subHeadLine = mainContent.append('h1','subHeadLine')
+    .content('- who makes data alive -')
+    .style('position','absolute')
+    .style('color','#eccc68')
+    .style('width','100%')
+    .style('height', '30px')
+    .style('text-align','center')
+    .style('left','0')
+    .style('right','0')
+    .style('bottom','30%')
+    .style('margin','auto')
+    .style('font-size','32px')
+    .style('transition', '1s')
+    .style('text-shadow','0 0 10px #eccc68')
+    .style('z-index',1);
+var infoButtonHalo = mainContent.append('span','infoButtonHalo')
+    .style('position','absolute')
+    .style('color','#eccc68')
+    .style('width','32px')
+    .style('height', '32px')
+    .style('overflow','hidden')
+    .style('border-radius','50%')
+    .style('text-align','center')
+    .style('left','0')
+    .style('right','0')
+    .style('bottom','18%')
+    .style('margin','auto')
+    .style('font-size','24px')
+    .style('transition', '0.5s')
+    .style('border','1px solid #eccc68')
+    .style('box-shadow','0 0 30px #eccc68')
+    .style('cursor','pointer')
+    .style('z-index',1);
+var infoButton = mainContent.append('span','infoButton')
+    .content('Wanna know more?')
+    .style('position','absolute')
+    .style('color','#eccc68')
+    .style('width','32px')
+    .style('height', '32px')
+    .style('overflow','hidden')
+    .style('border','1px solid transparent')
+    .style('border-radius','50%')
+    .style('text-align','center')
+    .style('left','0')
+    .style('right','0')
+    .style('bottom','18%')
+    .style('margin','auto')
+    .style('font-size','24px')
+    .style('transition', '0.5s')
+    .style('background', '#eccc68')
+    .style('text-shadow','0 0 5px #eccc68')
+    .style('box-shadow','0 0 10x #eccc68')
+    .style('cursor','pointer')
+    .style('z-index',1)
+    .on('mouseover',function () {
+        this.style('background','rgba(47, 54, 64,0.5)')
+            .style('width','100%')
+            .style('box-shadow','')
+            .style('border-radius','4px')
+        infoButton.overed = true;
+    })
+    .on('mouseleave',function () {
+        this.style('background', '#eccc68')
+            .style('width','32px')
+            .style('box-shadow','0 0 10px #eccc68')
+            .style('border-radius','50%')
+        infoButton.overed = false;
+    });
+var infoButtonHaloAn = AXR.append('infoButtonHaloAn')
+    .attr('freq',60)
+    .attr('action',function () {
+        if(infoButton.overed === true){
+            infoButtonHalo.state = 0;
+        }
+        switch(infoButtonHalo.state){
+            case undefined:
+            case 0:
+                infoButtonHalo.state = 1;
+                infoButtonHalo.style('width', '64px')
+                    .style('bottom','calc(18% - 15px)')
+                    .style('height', '64px')
+                    .style('opacity', 0);
+                break;
+            case 1:
+                infoButtonHalo.state = 2;
+                infoButtonHalo.style('width', '32px')
+                    .style('bottom','18%')
+                    .style('height', '32px');
+                break;
+            case 2:
+                infoButtonHalo.state = 0;
+                infoButtonHalo.style('opacity', 1);
+                break;
+        }
+    }).insert();
+
+
 var circleNum = 150;
 var circleList = [];
 var origin = {
